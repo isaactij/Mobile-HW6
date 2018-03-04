7ac74fbcee9f01ac570c46c450f45f7ad5be93d7
Isaac <isaactij@utexas.edu>
2018-03-04 17:38:27 -0600

    Right number of pages and swipes continuously between all of them

---
+++ TijerinaIsaac-hw6/TijerinaIsaac-hw6/ViewController.swift
@@ -0,0 +1,35 @@
+//
+//  ViewController.swift
+//  TijerinaIsaac-hw6
+//
+//  Created by Isaac on 3/3/18.
+//  Copyright © 2018 TijerinaIsaac. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}

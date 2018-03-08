4fe1329e285047c2e8ed1a6e20cf1a40f474c68f
Isaac <>
2018-03-08 02:46:25 -0600

    Complete

--- TijerinaIsaac-hw6/TijerinaIsaac-hw6/MyPageViewController.swift
+++ TijerinaIsaac-hw6/TijerinaIsaac-hw6/MyPageViewController.swift
@@ -9,29 +9,37 @@
 import UIKit
 
 class MyPageViewController: UIPageViewController {
+    //Var that holds the view controller pages
+    fileprivate(set) lazy var orderedViewControllers: [ViewController] = {
+        return [self.newViewController(number:1),
+               self.newViewController(number:2),
+               self.newViewController(number:3),
+               self.newViewController(number:4),
+               self.newViewController(number:5),
+               self.newViewController(number:6),
+               self.newViewController(number:7)]
+        }() as! [ViewController]
     
-    fileprivate(set) lazy var orderedViewControllers: [UIViewController] = {
-       return [self.newViewController(),
-               self.newViewController(),
-               self.newViewController(),
-               self.newViewController(),
-               self.newViewController(),
-               self.newViewController(),
-               self.newViewController()]
-        }()
-    
+    //Sets the transition style
     required init?(coder aDecoder: NSCoder) {
         super.init(transitionStyle: .scroll, navigationOrientation: .horizontal, options: nil)
     }
     
+    //First thing to run
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.title = "Page Controller"
+        //Sets title
+        self.title = "Wonders of the World"
         dataSource = self
+        //Sets the pages
         if let firstViewController = orderedViewControllers.first {
             setViewControllers([firstViewController], direction: .forward, animated: true, completion: nil)
         }
-        
+        //Sets the colors of the page bar
+        let pageControl = UIPageControl.appearance()
+        pageControl.pageIndicatorTintColor = UIColor.white
+        pageControl.currentPageIndicatorTintColor = UIColor.green
+        pageControl.backgroundColor = UIColor.lightGray
         
     }
 
@@ -40,24 +48,18 @@ class MyPageViewController: UIPageViewController {
         // Dispose of any resources that can be recreated.
     }
     
-    fileprivate func newViewController() -> UIViewController {
-        return UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "VC")
+    //When called returns a new viewcontroller with UIimage
+    fileprivate func newViewController(number:Int) -> UIViewController {
+        let viewController:ViewController = UIStoryboard(name: "Main", bundle: nil).instantiateViewController(withIdentifier: "VC") as! ViewController
+        viewController.indexP = number
+        return viewController
     }
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 }
 
+//Sets which view controller is previous to the current one
 extension MyPageViewController: UIPageViewControllerDataSource {
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerBefore viewController: UIViewController) -> UIViewController? {
-        guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else {
+        guard let viewControllerIndex = orderedViewControllers.index(of: viewController as! ViewController) else {
             return nil
         }
         var previousIndex = viewControllerIndex - 1
@@ -70,8 +72,9 @@ extension MyPageViewController: UIPageViewControllerDataSource {
         return orderedViewControllers[previousIndex]
     }
     
+    //Sets which view controller is agter the current one
     func pageViewController(_ pageViewController: UIPageViewController, viewControllerAfter viewController: UIViewController) -> UIViewController? {
-        guard let viewControllerIndex = orderedViewControllers.index(of: viewController) else {
+        guard let viewControllerIndex = orderedViewControllers.index(of: viewController as! ViewController) else {
             return nil
         }
         var nextIndex = viewControllerIndex + 1
@@ -89,7 +92,7 @@ extension MyPageViewController: UIPageViewControllerDataSource {
     }
     
     func presentationIndex(for pageViewController:UIPageViewController) -> Int {
-        guard let firstViewController = viewControllers?.first, let firstViewControllerIndex = orderedViewControllers.index(of: firstViewController) else {
+        guard let firstViewController = viewControllers?.first, let firstViewControllerIndex = orderedViewControllers.index(of: firstViewController as! ViewController) else {
             return 0
         }
         
--- TijerinaIsaac-hw6/TijerinaIsaac-hw6/ViewController.swift
+++ TijerinaIsaac-hw6/TijerinaIsaac-hw6/ViewController.swift
@@ -12,25 +12,23 @@ class ViewController: UIViewController {
     @IBOutlet weak var imageView: UIImageView!
     var indexP:Int  = 1
     
+    //First thing to be called
     override func viewDidLoad() {
         super.viewDidLoad()
-        imageView.image = UIImage(named: "wonders\(index)")
+        //Begins adjusting the constraints
+        let margins = view.layoutMarginsGuide
+        // Pin the leading edge of myView to the margin's leading edge
+        imageView.leadingAnchor.constraint(equalTo: margins.leadingAnchor).isActive = true
+        // Pin the trailing edge of myView to the margin's trailing edge
+        imageView.trailingAnchor.constraint(equalTo: margins.trailingAnchor).isActive = true
+        // Give myView a 1:2 aspect ratio
+        imageView.heightAnchor.constraint(equalTo: imageView.widthAnchor, multiplier: 2.0).isActive = true
+        //Sets the image
+        imageView.image = UIImage(named: "wonders\(indexP)")
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
-
 }
